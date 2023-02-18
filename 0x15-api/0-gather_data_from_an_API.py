1a631cf6aeee4b61b65b5c8f265e515e977c40eb
markbonchi <markbonchi@gmail.com>
2023-02-18 23:57:40 +0000

    Gathering data

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,28 +7,29 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-  arg = int(argv[1])
-# print(arg)
-  url = 'https://jsonplaceholder.typicode.com/'
-  users = requests.get(url + 'users')
-  todos = requests.get(url + 'todos')
-# print(users.json())
+	arg = int(argv[1])
 
-  for item in users.json():
-# print(item)
-    if arg == item.get("id"):
-#   print(item)
-      total = 0
-      count = 0
-      name = item.get("name")
-      task = []
-      for i in todos.json():
-#     print(i)
-        if arg == i.get("userId"):
-          total += 1
-          if i.get("completed"):
-            task.append(i.get("title"))
-            count += 1
-      print("Employee {} is done with tasks({}/{}):".format(name, count, total))
-      for val in task:
-        print("\t {}".format(val))
+	url = 'https://jsonplaceholder.typicode.com/'
+	users = requests.get(url + 'users')
+	todos = requests.get(url + 'todos')
+#   	print(users.json())
+
+	for item in users.json():
+#    	print(item)
+		if arg == item.get("id"):
+#   	    	print(item)
+			total = 0
+			count = 0
+			name = item.get("name")
+			task = []
+			for i in todos.json():
+#     				print(i)
+				if arg == i.get("userId"):
+					total += 1
+					if i.get("completed"):
+						task.append(i.get("title"))
+						count += 1
+			print("""Employee {} is done with tasks({}/{}):\
+			""".format(name, count, total))
+			for val in task:
+				print("\t {}".format(val))
