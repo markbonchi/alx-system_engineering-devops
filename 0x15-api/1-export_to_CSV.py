1c5f101bb565548b6e5b54e39f81b190bb959303
markbonchi <markbonchi@gmail.com>
2023-02-19 01:14:39 +0000

    Export to csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,27 +1,20 @@
 #!/usr/bin/python3
 """Module that returns information about TODO list progress
 """
-
-
+import csv
 import requests
 from sys import argv
 
 if __name__ == "__main__":
 	arg = int(argv[1])
-
-	file = "USER_ID.csv"
 	url = 'https://jsonplaceholder.typicode.com/'
-	users = requests.get(url + 'users')
-	todos = requests.get(url + 'todos')
+	user = requests.get(url + 'users/{}'.format(arg)).json()
+	todos = requests.get(url + 'todos', params={'userId': arg}).json()
+	name = user.get("username")
 
-	for item in users.json():
-		if arg == item.get("id"):
-			name = item.get("name")
-			for i in todos.json():
-				if arg == i.get("userId"):
-					with open(file, mode='a', encoding="utf-8") as f:
-						f.write('''"{}","{}","{}","{}"\n'''\
-					   		.format(arg,
-						   		name,
-						   		i.get("completed"),
-						   		i.get("title")))
+	with open("{}.csv".format(arg), 'w', newline='') as csvfile:
+		writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+		[writer.writerow(
+			[arg, name, i.get("completed"), i.get("title")]
+		) for i in todos]
+
