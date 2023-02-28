31ec8f6dde5400185a4e2d13253aa3b84a308333
markbonchi <markbonchi@gmail.com>
2023-02-28 12:02:27 +0000

    Fix to 0-subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,6 +7,7 @@ def number_of_subscribers(subreddit):
     """returns the number of subscribers for a given subreddit."""
     if subreddit is None or type(subreddit) is not str:
         return 0
-    r = requests.get("https://www.reddit.com/r/{}/about.json".format(subreddit)).json()
+    r = requests.get("https://www.reddit.com/r/{}/about.json\
+".format(subreddit)).json()
     subs = r.get("data", {}).get("subscribers", 0)
     return subs
